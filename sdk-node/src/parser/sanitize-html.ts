cb1532e34cbe51a62873fcddd67c9787a95b308d
Ethan Byrd <contact@etbyrd.com>
2026-04-12 09:22:27 -0700

    fix(node): use spaces not tabs in sanitize-html.ts

--- sdk-node/src/parser/sanitize-html.ts
+++ sdk-node/src/parser/sanitize-html.ts
@@ -1,136 +1,136 @@
 import DOMPurify from "isomorphic-dompurify";
 
 const ALLOWED_TAGS = [
-	// Structure
-	"div",
-	"span",
-	"p",
-	"br",
-	"hr",
-	// Text formatting
-	"b",
-	"i",
-	"u",
-	"strong",
-	"em",
-	"small",
-	"sub",
-	"sup",
-	"s",
-	"strike",
-	// Headings
-	"h1",
-	"h2",
-	"h3",
-	"h4",
-	"h5",
-	"h6",
-	// Lists
-	"ul",
-	"ol",
-	"li",
-	"dl",
-	"dt",
-	"dd",
-	// Tables
-	"table",
-	"thead",
-	"tbody",
-	"tfoot",
-	"tr",
-	"th",
-	"td",
-	"colgroup",
-	"col",
-	"caption",
-	// Links and images
-	"a",
-	"img",
-	// Semantic
-	"blockquote",
-	"pre",
-	"code",
-	"address",
-	"center",
-	// Legacy but common in email
-	"font",
-	"big",
+  // Structure
+  "div",
+  "span",
+  "p",
+  "br",
+  "hr",
+  // Text formatting
+  "b",
+  "i",
+  "u",
+  "strong",
+  "em",
+  "small",
+  "sub",
+  "sup",
+  "s",
+  "strike",
+  // Headings
+  "h1",
+  "h2",
+  "h3",
+  "h4",
+  "h5",
+  "h6",
+  // Lists
+  "ul",
+  "ol",
+  "li",
+  "dl",
+  "dt",
+  "dd",
+  // Tables
+  "table",
+  "thead",
+  "tbody",
+  "tfoot",
+  "tr",
+  "th",
+  "td",
+  "colgroup",
+  "col",
+  "caption",
+  // Links and images
+  "a",
+  "img",
+  // Semantic
+  "blockquote",
+  "pre",
+  "code",
+  "address",
+  "center",
+  // Legacy but common in email
+  "font",
+  "big",
 ];
 
 const ALLOWED_ATTRS = [
-	"class",
-	"id",
-	"dir",
-	"lang",
-	"href",
-	"title",
-	"target",
-	"rel",
-	"src",
-	"alt",
-	"width",
-	"height",
-	"border",
-	"cellpadding",
-	"cellspacing",
-	"align",
-	"valign",
-	"bgcolor",
-	"colspan",
-	"rowspan",
-	"span",
-	"color",
-	"size",
-	"face",
+  "class",
+  "id",
+  "dir",
+  "lang",
+  "href",
+  "title",
+  "target",
+  "rel",
+  "src",
+  "alt",
+  "width",
+  "height",
+  "border",
+  "cellpadding",
+  "cellspacing",
+  "align",
+  "valign",
+  "bgcolor",
+  "colspan",
+  "rowspan",
+  "span",
+  "color",
+  "size",
+  "face",
 ];
 
 const ALLOWED_URI_REGEXP = /^(data:|mailto:|#)/i;
 
 export function sanitizeHtml(html: string): string {
-	DOMPurify.addHook("uponSanitizeAttribute", (_node, data) => {
-		if (data.attrName.startsWith("on")) {
-			data.keepAttr = false;
-		}
-		if (data.attrName === "style") {
-			data.keepAttr = false;
-		}
-	});
+  DOMPurify.addHook("uponSanitizeAttribute", (_node, data) => {
+    if (data.attrName.startsWith("on")) {
+      data.keepAttr = false;
+    }
+    if (data.attrName === "style") {
+      data.keepAttr = false;
+    }
+  });
 
-	DOMPurify.addHook("afterSanitizeAttributes", (node) => {
-		if (node.tagName === "A") {
-			const target = node.getAttribute("target");
-			if (target === "_blank") {
-				node.setAttribute("rel", "noopener noreferrer");
-			}
-		}
-	});
+  DOMPurify.addHook("afterSanitizeAttributes", (node) => {
+    if (node.tagName === "A") {
+      const target = node.getAttribute("target");
+      if (target === "_blank") {
+        node.setAttribute("rel", "noopener noreferrer");
+      }
+    }
+  });
 
-	const result = DOMPurify.sanitize(html, {
-		ALLOWED_TAGS,
-		ALLOWED_ATTR: ALLOWED_ATTRS,
-		ALLOW_DATA_ATTR: false,
-		ALLOW_UNKNOWN_PROTOCOLS: false,
-		ALLOWED_URI_REGEXP,
-		FORBID_TAGS: [
-			"style",
-			"script",
-			"iframe",
-			"object",
-			"embed",
-			"form",
-			"input",
-			"button",
-			"select",
-			"textarea",
-			"link",
-			"meta",
-			"base",
-			"svg",
-			"math",
-		],
-	});
+  const result = DOMPurify.sanitize(html, {
+    ALLOWED_TAGS,
+    ALLOWED_ATTR: ALLOWED_ATTRS,
+    ALLOW_DATA_ATTR: false,
+    ALLOW_UNKNOWN_PROTOCOLS: false,
+    ALLOWED_URI_REGEXP,
+    FORBID_TAGS: [
+      "style",
+      "script",
+      "iframe",
+      "object",
+      "embed",
+      "form",
+      "input",
+      "button",
+      "select",
+      "textarea",
+      "link",
+      "meta",
+      "base",
+      "svg",
+      "math",
+    ],
+  });
 
-	DOMPurify.removeAllHooks();
+  DOMPurify.removeAllHooks();
 
-	return result;
+  return result;
 }
